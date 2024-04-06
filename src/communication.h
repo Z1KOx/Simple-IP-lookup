512d6cbe7aaeb7f571ec220736847f40816edd81
Z1KOx <119358594+Z1KOx@users.noreply.github.com>
2024-04-06 23:07:25 +0200

    small header fix

--- src/communication.h
+++ src/communication.h
@@ -4,7 +4,7 @@
 #include <curl/curl.h>
 #include <cJSON/cJSON.h>
 
-size_t write_callback(const char* ptr, const size_t size, const size_t nmemb, const void* userData);
+size_t writeCallback(const char* ptr, const size_t size, const size_t nmemb, const void* userData);
 char* getResponse(const char* ipAddress);
 void printJson(const char* response, const char* name);
 
